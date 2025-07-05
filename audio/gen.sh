2184f53120584f9a4fe9e91bb3f7f51dc3250d1c
Monty Zukowski <monty@washdish.com>
2025-07-05 09:46:08 -0700

    up

---
+++ audio/gen.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+gtts-cli 'don' -l ja --output don.mp3
+gtts-cli 'ka' -l ja --output ka.mp3
+gtts-cli 'ラ' -l ja --output ra.mp3
+gtts-cli 'ku' -l ja --output ku.mp3
+gtts-cli 'tsu' -l ja --output tsu.mp3
+gtts-cli 'do' -l ja --output do.mp3
+gtts-cli 'ko' -l ja --output ko.mp3
+gtts-cli 'su' -l ja --output su.mp3
+gtts-cli 'ソレ' -l ja --output sore.mp3
+gtts-cli 'kri' -l en --output kri.mp3
