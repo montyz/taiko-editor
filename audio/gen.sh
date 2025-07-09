b3f87b5eddebeebef2c8a577649e049389fd9132
Monty Zukowski <monty@washdish.com>
2025-07-09 09:29:25 -0700

    up

--- audio/gen.sh
+++ audio/gen.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-gtts-cli 'don' -l ja --output don.mp3
-gtts-cli 'ka' -l ja --output ka.mp3
-gtts-cli 'ラ' -l ja --output ra.mp3
-gtts-cli 'ku' -l ja --output ku.mp3
-gtts-cli 'tsu' -l ja --output tsu.mp3
-gtts-cli 'do' -l ja --output do.mp3
-gtts-cli 'ko' -l ja --output ko.mp3
-gtts-cli 'su' -l ja --output su.mp3
-gtts-cli 'ソレ' -l ja --output sore.mp3
-gtts-cli 'kri' -l en --output kri.mp3
-gtts-cli 'two' -l en --output two.mp3
-gtts-cli 'three' -l en --output three.mp3
-gtts-cli 'four' -l en --output four.mp3
+gtts-cli 'don' -l ja --output don.wav
+gtts-cli 'ka' -l ja --output ka.wav
+gtts-cli 'ラ' -l ja --output ra.wav
+gtts-cli 'ku' -l ja --output ku.wav
+gtts-cli 'tsu' -l ja --output tsu.wav
+gtts-cli 'do' -l ja --output do.wav
+gtts-cli 'ko' -l ja --output ko.wav
+gtts-cli 'su' -l ja --output su.wav
+gtts-cli 'ソレ' -l ja --output sore.wav
+gtts-cli 'kri' -l en --output kri.wav
+gtts-cli 'two' -l en --output two.wav
+gtts-cli 'three' -l en --output three.wav
+gtts-cli 'four' -l en --output four.wav
 
